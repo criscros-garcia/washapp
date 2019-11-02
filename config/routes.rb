0a30094a8eccfe94bd6e3c5df06cccd9311c4e24
XolotlP <xolotl.mp@gmail.com>
2019-11-02 11:40:55 -0600

    Add devise model to create users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    passwords: 'users/passwords',
+    registrations: 'users/registrations'
+  }
+
   resources :packages
 
   root 'welcome#index'
